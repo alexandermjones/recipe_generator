bb6fdd26d7e46c0ee6f9701815b16a2754935590
Alex Jones <amjones@live.co.uk>
2021-03-06 21:24:09 +0000

    Adds pretrained models

--- recipes.py
+++ recipes.py
@@ -4,10 +4,19 @@ import os
 # Third party imports
 from textgenrnn import textgenrnn
 
-# Train model and generate new recipes
-path = os.path.join("datasets", "bbcgoodfood.txt")
-#textgen = textgenrnn()
-#textgen.train_from_file(path, num_epochs=5, save_epochs=1, name="recipes")
-textgen = textgenrnn('textgenrnn_weights.hdf5')
-print(textgen.generate(5, temperature=0.5, return_as_list=True, prefix="Chocolate cake"))
-#textgen.generate(interactive=True, temperature=0.5)
+def train_model_on_data(data_path: str):
+    """
+    """
+    name = os.path.join("models", os.path.basename(data_path)[:-4])
+    textgen = textgenrnn()
+    textgen.train_from_file(data_path, num_epochs=5, save_epochs=1, name=name)
+    textgen.save(weights_path=name+".hdf5")
+
+# Uncomment to train a new model
+# train_model_on_data(os.path.join("data", "dessert.txt"))
+# train_model_on_data(os.path.join("data", "breakfast.txt"))
+# train_model_on_data(os.path.join("data", "dinner.txt"))
+
+textgen = textgenrnn(os.path.join("models", "dinner.hdf5"))
+recipes = textgen.generate(20, temperature=0.5, return_as_list=True)
+[print(recipe) for recipe in recipes]
